06113791e238862c294527ba4fc866934007777f
Condor3397 <freedomcondor@126.com>
2019-03-08 15:43:56 +0100

    try add a new repository for builderbot caliberation

---
+++ coding.sh
@@ -0,0 +1,5 @@
+vim coding.sh -c "
+	tabnew src/testing/test_builderbot.argos
+	tabnew src/testing/builderbot.lua
+	tabnew src/testing/block.lua
+"
---
+++ run.sh
@@ -0,0 +1,14 @@
+argosBuildDir="build"
+
+#this if may not be necessary
+if [ "$ARGOS_PLUGIN_PATH" == "" ]; then 
+	echo "sourcing"
+	source $argosBuildDir/setup_env.sh
+fi
+#$argosBuildDir/core/argos3 -c experiment_share_clustering.argos
+#$argosBuildDir/core/argos3 -c experiment_cmd_clustering.argos
+#$argosBuildDir/core/argos3 -c experiment_surround.argos
+#$argosBuildDir/core/argos3 -c experiment_connection.argos
+$argosBuildDir/core/argos3 -c src/testing/test_builderbot.argos
+			
+
